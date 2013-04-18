ca6d7ab7e9e692b6e9fbf9ceee087cd82982f715
Tai Lee <tai.lee@3030.com.au>
2013-04-18 21:05:43 +1000

    Report all URLs that were tried, not only the `multiurl()` patterns, when `Resolver404` is raised in `multiview()`.

--- multiurl.py
+++ multiurl.py
@@ -44,15 +44,16 @@ class MultiRegexURLResolver(urlresolvers.RegexURLResolver):
                     patterns_matched.append([pattern])
                 tried.append([pattern])
         if matched:
-            return MultiResolverMatch(matched, self._exceptions, patterns_matched, path)
+            return MultiResolverMatch(matched, self._exceptions, patterns_matched, path, tried)
         raise urlresolvers.Resolver404({'tried': tried, 'path': path})
 
 class MultiResolverMatch(object):
-    def __init__(self, matches, exceptions, patterns_matched, path):
+    def __init__(self, matches, exceptions, patterns_matched, path, tried):
         self.matches = matches
         self.exceptions = exceptions
         self.patterns_matched = patterns_matched
         self.path = path
+        self.tried = tried
 
         # Attributes to emulate ResolverMatch
         self.kwargs = {}
@@ -69,6 +70,6 @@ class MultiResolverMatch(object):
                     return match.func(request, *match.args, **match.kwargs)
                 except self.exceptions:
                     continue
-            raise urlresolvers.Resolver404({'tried': self.patterns_matched, 'path': self.path})
+            raise urlresolvers.Resolver404({'tried': self.tried, 'path': self.path})
         multiview.multi_resolver_match = self
         return multiview
